45089f9403894dae471a3d4cd4df2532cb0ed103
Amodu Kehinde <amodukehindee@gmail.com>
2022-10-11 16:40:27 +0100

    done with features

--- components/Home/HomePage.tsx
+++ components/Home/HomePage.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
-import { FaTimes, FaGripLines, FaHeart, FaCheckCircle } from 'react-icons/fa';
-import { AiOutlineUser, AiOutlineSearch, AiOutlineShopping} from 'react-icons/ai';
+import { FaTimes, FaGripLines, FaCheckCircle } from 'react-icons/fa';
+import { AiOutlineUser, AiOutlineSearch, AiOutlineShopping, AiOutlineHeart} from 'react-icons/ai';
 import Image from 'next/image';
 
 export default function HomePage() {
@@ -12,7 +12,7 @@ export default function HomePage() {
 					<div>
 						<Image width='50%' height='40%' src='/images/logo.svg' alt='logo' />
 					</div>
-					<div className='space-x-6 hidden text-[16px] lg:block'>
+					<div className='space-x-6 font-medium hidden text-[16px] lg:block'>
 						<Link href=''>Footwear</Link>
 						<Link href=''>About us</Link>
 						<Link href=''>Products</Link>
@@ -36,7 +36,7 @@ export default function HomePage() {
 				<header className='my-8 grid grid-flow-row md:items-center md:grid-flow-col'>
 					<div className='flex flex-col items-center md:items-start gap-8'>
 						<div className='mt-8 md:mt-0'>
-							<h4 className='text-[32px] md:max-w-lg lg:text-[44px] font-bold md:text-start text-center py-4'>
+							<h4 className='text-[32px] md:text-[25px] md:max-w-md lg:text-[44px] font-bold md:text-start text-center py-4'>
 								Get your awesome sneakers.
 							</h4>
 							<p className='text-[18px] md:text-start max-w-sm md:max-w-sm text-center text-[#6C6B6B]'>
@@ -48,31 +48,154 @@ export default function HomePage() {
 							<button className='bg-[#FF3C78] font-bold text-white w-[125px] lg:w-[160px] h-[48px] rounded-[4px]'>
 								Shop Now
 							</button>
-							<FaHeart />
+							<span className='text-[#FF3C78] text-[24px]'>
+								<AiOutlineHeart />
+							</span>
 						</div>
-						<div className='flex flex-col'>
+						<div className='flex flex-col md:flex-row md:items-center md:gap-8 space-y-2'>
 							<span className='flex flex-row items-center space-x-2'>
-								<FaCheckCircle />
-								<p>Free shipping</p>
+								<span className='text-green-600 text-[20px]'>
+									<FaCheckCircle />
+								</span>
+								<p className='text-[16px] text-[#6C6B6B]'>Free shipping</p>
 							</span>
 							<span className='flex flex-row items-center space-x-2'>
-								<FaCheckCircle />
-								<p>Free returns</p>
+								<span className='text-green-600 text-[20px]'>
+									<FaCheckCircle />
+								</span>
+								<p className='text-[16px] text-[#6C6B6B]'>Free returns</p>
 							</span>
 						</div>
 					</div>
-					<div>
+					<div className=''>
 						<Image
-							width='1000px'
-							height='1000px'
+							width='800px'
+							height='800px'
 							objectFit='cover'
 							src='/images/Sha.svg'
 							alt='logo'
-							// className='w-[800px] h-[800px]'
 						/>
 					</div>
 				</header>
 			</div>
+			<main className='w-full h-screen px-8'>
+				<div className='my-12'>
+					<h4 className='text-[26px] font-bold text-center'>
+						Featured products
+					</h4>
+					<p></p>
+				</div>
+				<div className='py-8 flex flex-col gap-24 md:gap-12 md:flex-auto lg:flex-row'>
+					<div className='flex flex-col items-center'>
+						<div className='relative top-0 left-0 right-0 bottom-0 flex flex-col items-center'>
+							<div className='absolute left-24 md:left-36 lg:left-24 lg:w-[80px] lg:h-[80px] -top-6 right-0 bg-gradient-to-r rounded-full w-[120px] h-[120px]  from-[#FFDEBE] to-[#FE7831]'></div>
+							<div className=''>
+								<Image
+									width='400px'
+									height='200px'
+									objectFit='cover'
+									src='/images/shoe1.svg'
+									alt='logo'
+								/>
+							</div>
+						</div>
+						<div className='text-center space-y-4'>
+							<Image
+								width='90px'
+								height='20px'
+								objectFit='contain'
+								src='/images/Rating.svg'
+								alt='logo'
+							/>
+							<p className='text-[16px] max-w-md'>
+								Adidas Falcon Shoes for men - 2021 Edition
+							</p>
+							<h4 className='font-bold text-[18px]'>$120.50</h4>
+						</div>
+					</div>
+					<div className='flex flex-col items-center'>
+						<div className='relative flex flex-col items-center'>
+							<div className='absolute left-24 md:left-36 lg:left-24 lg:w-[80px] lg:h-[80px]  -top-6 right-0 bg-gradient-to-r rounded-full w-[120px] h-[120px]  from-[#50ad89] to-[ #02BE83]'></div>
+							<div className=''>
+								<Image
+									width='400px'
+									height='200px'
+									objectFit='cover'
+									src='/images/shoe2.svg'
+									alt='logo'
+								/>
+							</div>
+						</div>
+						<div className='text-center space-y-4'>
+							<Image
+								width='90px'
+								height='20px'
+								objectFit='contain'
+								src='/images/Rating.svg'
+								alt='logo'
+							/>
+							<p className='text-[16px] max-w-md'>
+								Adidas Falcon Shoes for men - 2021 Edition
+							</p>
+							<h4 className='font-bold text-[18px]'>$120.50</h4>
+						</div>
+					</div>
+					<div className='flex flex-col items-center'>
+						<div className='relative flex flex-col items-center'>
+							<div className='absolute left-24 md:left-36 lg:left-24 lg:w-[80px] lg:h-[80px]  -top-6 right-0 bg-gradient-to-r rounded-full w-[120px] h-[120px]  from-[#FFB2B2] to-[#FF3C78]'></div>
+							<div className=''>
+								<Image
+									width='400px'
+									height='200px'
+									objectFit='cover'
+									src='/images/shoe3.svg'
+									alt='logo'
+								/>
+							</div>
+						</div>
+						<div className='text-center space-y-4'>
+							<Image
+								width='90px'
+								height='20px'
+								objectFit='contain'
+								src='/images/Rating.svg'
+								alt='logo'
+							/>
+							<p className='text-[16px] max-w-md'>
+								Adidas Falcon Shoes for men - 2021 Edition
+							</p>
+							<h4 className='font-bold text-[18px]'>$120.50</h4>
+						</div>
+					</div>
+					<div className='flex flex-col items-center'>
+						<div className='relative flex flex-col items-center'>
+							<div className='absolute left-24 md:left-36 lg:left-24 lg:w-[80px] lg:h-[80px]  -top-6 right-0 bg-gradient-to-r rounded-full w-[120px] h-[120px]  from-[#315BFF] to-[#8AA8F8]'></div>
+							<div className=''>
+								<Image
+									width='400px'
+									height='200px'
+									objectFit='cover'
+									src='/images/shoe4.svg'
+									alt='logo'
+								/>
+							</div>
+						</div>
+						<div className='text-center space-y-4'>
+							<Image
+								width='90px'
+								height='20px'
+								objectFit='contain'
+								src='/images/Rating.svg'
+								alt='logo'
+							/>
+							<p className='text-[16px] max-w-md'>
+								Adidas Falcon Shoes for men - 2021 Edition
+							</p>
+							<h4 className='font-bold text-[18px]'>$120.50</h4>
+						</div>
+					</div>
+				</div>
+			</main>
 		</>
 	);
 }
